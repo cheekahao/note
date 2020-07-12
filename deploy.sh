e85e0264ffba82639a2fe21c87b36d7c160de965
zhenjia.hao <zhenjia.hao@tendcloud.com>
2020-07-12 16:51:52 +0800

    build: change repo learning to note

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cheekahao/learning.git master:gh-pages
+git push -f git@github.com:cheekahao/note.git master:gh-pages
 
 cd -
