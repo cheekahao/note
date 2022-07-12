7c7207ddf64d9df04f7a4f35785b49b5b28adee4
cheeka <cheeka@163.com>
2022-07-12 17:16:32 +0800

    feat: 完善rollup

--- docs/engineering/rollup.md
+++ docs/engineering/rollup.md
@@ -24,14 +24,77 @@
 
 ```mermaid
 flowchart LR
-    rollup -c
+    A("rollup -c") --> B("解析命令行参数") --> C("run(command)执行对应命令")
 ```
 
-以下三个大的阶段：
+### 构建(build)阶段
 
-* 输入(input)阶段，运行`cli`命令后，解析命令行参数，调用`run(command)`
-* 构建(build)阶段，在`cli/build.js`中调用`Rollup.rollup(inputOptions)`得到一个`bundle`
-* 输出(output)阶段，调用`bundle.generate(output)`，将返回的`outputs`
+整个构建阶段，主要是在`cli/build.js`中调用`Rollup.rollup(inputOptions)`得到一个`bundle`的过程，具体过程如下：
 
+```mermaid
+flowchart TB
+    A("build(inputOptions)") --> B("Rollup.rollup(inputOptions)") --> C("new Graph(inputOptions)") --buildStart--> D("graph.build()") --buildEnd--> E("return result")
+```
 
-* 调用
+最后返回的是一个包含`generate`方法和`write`的类型为`RollupBuild`的对象。其类型定义如下：
+
+```ts
+export interface RollupBuild {
+	cache: RollupCache | undefined;
+	close: () => Promise<void>;
+	closed: boolean;
+	generate: (outputOptions: OutputOptions) => Promise<RollupOutput>;
+	getTimings?: () => SerializedTimings;
+	watchFiles: string[];
+	write: (options: OutputOptions) => Promise<RollupOutput>;
+}
+```
+
+### 输出(output)阶段
+
+输出(output)阶段，主要通过调用`bundle.generate(output)`完成，`bundle.generate`方法在`Rollup.rollup`的返回值中。具体过程如下：
+
+```mermaid
+flowchart TB
+    A("generate(output)") --> B("getOutputOptionsAndPluginDriver") --> C("new Bundle()") --> D("bundle.generate(isWrite)")  --> E{isWrite} --是--> F("Object.values(generated)\n.map(chunk => writeOutputFile(chunk, outputOptions))") --writeBundle--> G("createOutput(generated)")
+    E --否--> G
+```
+
+## 插件Plugin
+
+`Rollup`插件是一个具有约定好的**属性**、**构建钩子**或**输出生成钩子**的对象，可以用来定制`Rollup`的行为。
+
+因此一个`Rollup`插件中最主要的就是基于约定的钩子函数。
+
+### 钩子函数的调用时机
+
+钩子函数的调用时机有三类：
+
+* `Build Hooks`，`Rollup.rollup(inputOptions)`执行的构建钩子函数
+* `Output Generation Hooks`，`bundle.generate()`期间执行的输出钩子函数
+* `Watch Change Hooks`， `rollup.watch`执行期间监听文件变化并重新执行构建时执行的钩子函数
+
+### 钩子函数处理方式分类
+
+钩子函数的调用主要由`PluginDriver`负责，主要有以下四种类型：
+
+* 异步`async`:  处理`promise`的异步钩子，即这类`hook`可以返回一个解析为相同类型值的`promise`，同步版本`hook`也将将被转换为`sync`
+* 第一个有返回值`first`:  如果多个插件实现了相同的钩子函数，那么会串式执行，从头到尾，但是，如果其中某个的返回值不是`null`也不是 `undefined`的话，会直接终止掉后续插件
+* 连续`sequential`:  如果多个插件实现了相同的钩子函数，那么会串式执行，按照使用插件的顺序从头到尾执行，如果是异步的，会等待之前处理完毕，在执行下一个插件
+* `parallel`:  同上，不过如果某个插件是异步的，其后的插件不会等待，而是并行执行，这个也就是我们在`rollup.rollup()`阶段看到的处理方式。
+
+### 构建钩子函数
+
+构建钩子函数可以影响构建执行方式、提供构建的信息或者在构建完成后修改构建。
+
+构建钩子函数主要关注在`Rollup`处理输入文件之前定位、提供和转换输入文件。构建阶段的第一个钩子是`options`，最后一个钩子总是`buildEnd`。构建发生错误时，会触发`closeBundle`。
+
+在观察模式下，`watchChange`钩子可以在任何时候被触发，以通知新的运行将在当前运行产生其输出后被触发。当`watcher`关闭时，`closeWatcher`钩子函数将被触发。
+
+### 输出钩子函数
+
+输出生成钩子函数可以提供关于生成的包的信息并在构建完成后立马执行。
+
+输出生成阶段的第一个钩子函数是`outputOptions`，如果输出通过`bundle.generate()`成功生成则第一个钩子函数是 `generateBundle`，如果输出通过`bundle.write()`，另外如果输出生成阶段发生了错误的话，最后一个钩子函数则是 `renderError`。
+
+`closeBundle`可以作为最后一个钩子被调用，但用户有责任手动调用`bundle.close()`来触发它。`CLI`将始终确保这种情况发生。
--- test.js
+++ test.js
@@ -1,8 +1,222 @@
-const target = {}
-const proxy = new Proxy(target, {
-    get(){
-        return 12312
-    }
-})
+function mergeSort(array) {
+    const {
+        length
+    } = array
+    if (length < 2) return array
 
-console.log(proxy.a);
+    const middle = Math.floor(length / 2)
+    const left = mergeSort(array.slice(0, middle))
+    const right = mergeSort(array.slice(middle, length))
+    console.log('left', left, 'right', right);
+    return merge(left, right)
+}
+
+function merge(left, right) {
+    let i = 0;
+    let j = 0;
+    let result = []
+
+    while (i < left.length && j < right.length) {
+        const leftItem = left[i]
+        const rightItem = right[j]
+        let item
+
+        if (leftItem < rightItem) {
+            item = leftItem;
+            i++;
+        } else {
+            item = rightItem;
+            j++;
+        }
+        result.push(item)
+    }
+
+    result = result.concat(i < left.length ? left.slice(i) : right.slice(j))
+    console.log('result', result);
+    return result
+}
+
+function lengthOfLongestSubstring(s){
+    const arr = s.split('')
+    let maxLength = 0
+    const sub = []
+    const map = {}
+
+    while (arr.length) {
+        const item = arr.shift()
+
+        while (map[item] && sub.length) {
+            const first = sub.shift()
+
+            delete map[first]
+        }
+
+        map[item] = 1
+        sub.push(item)
+
+        maxLength = sub.length > maxLength ? sub.length : maxLength
+    }
+
+    return maxLength
+}
+
+function minWindow(s, t) {
+    const {length} = s
+    const total = t.length
+    const leftSteps = getContainedSteps(s, t)
+    const rightSteps = leftSteps.slice(0)
+    let left = leftSteps.shift()
+    let right = rightSteps.shift()
+    let minSubStr = ''
+    const sourceCountMap = {}
+    const targetCountMap = getCountMap(t)
+
+    while (right <= length) {
+        let rightChat = s.charAt(right)
+        let leftChat = s.charAt(left)
+        updateCountMap(sourceCountMap, rightChat)
+
+        while (right - left >= total && sourceCountMap[leftChat] > targetCountMap[leftChat]) {
+            reduceCountMap(sourceCountMap, leftChat)
+            left = leftSteps.shift()
+            leftChat = s.charAt(left)
+        }
+
+        const subStr = s.substring(left, right + 1)
+
+        if ((!minSubStr || minSubStr.length > subStr.length) && hasContainedSource(sourceCountMap, targetCountMap)) minSubStr = subStr
+
+        if (rightSteps.length){
+            right = rightSteps.shift()
+        }else {
+            right++
+        }
+
+    }
+
+    return minSubStr
+}
+
+function getContainedSteps(source, target) {
+    const steps = []
+    const {length} = source
+
+    for (let index = 0; index < length; index++) {
+        const char = source.charAt(index)
+
+        if (target.indexOf(char) > -1) steps.push(index)
+    }
+
+    return steps
+}
+
+function getCountMap(str) {
+    const map = {}
+    const {
+        length
+    } = str
+
+    for (let index = 0; index < length; index++) {
+        const char = str.charAt(index)
+
+        updateCountMap(map, char)
+    }
+
+    return map
+}
+
+function updateCountMap(map, char) {
+    map[char] = (map[char] || 0) + 1
+}
+function reduceCountMap(map, char) {
+    map[char] = map[char] > 0 ? map[char] - 1 : 0
+}
+
+function hasContainedSource(sourceCountMap, targetCountMap) {
+    let hasContained = true
+    for (const key in targetCountMap) {
+        if (Object.hasOwnProperty.call(targetCountMap, key)) {
+            const sourceCount = sourceCountMap[key] || 0
+            if (sourceCount < targetCountMap[key]){
+                hasContained = false
+                break
+            }
+
+        }
+    }
+
+    return hasContained
+}
+
+function minWindow2(s, t) {
+    const {
+        length
+    } = s
+    let left = 0
+    let right = left + total
+    const total = t.length
+    const set = new Set()
+    const map = {}
+    let minSubString = ''
+
+    while (right < length) {
+        let item = s.charAt(right)
+
+        right++
+
+        // 不在字串t中，直接进入下一循环
+        if (t.indexOf(item) == -1) continue
+
+        if (!set.has(item)) { //set中不包含item，向set、map中增加对应数据
+            set.add(item)
+            map[item] = 1 //用于保存字串中该字符的个数
+        } else {
+            map[item] += 1
+        }
+
+        const isAllContained = set.size == total //set的大小等于字符串长度，说明字串已经包含所有
+
+        if (!isAllContained) continue // 没有包含全部字符时，直接进入下一循环
+
+        // 开始移动做指针
+
+        let leftItem = s.charAt(left)
+        let leftCount = map[leftItem] || 0
+        while (leftCount !== 1) {
+            if (leftCount > 1) map[leftItem] -= 1
+
+            left++
+            leftItem = s.charAt(left)
+
+            leftCount = map[leftItem] || 0
+        }
+
+        const subString = s.substring(left, right)
+
+        if (minSubString.length > subString.length) {
+            minSubString = subString
+        }
+    }
+
+    if (set.size < total) return ''
+
+    return minSubString
+}
+
+
+var findMedianSortedArrays = function (nums1, nums2) {
+    const total = nums1.length + nums2.length
+    const isEven = total % 2 === 0
+    const middle = Math.ceil(total / 2)
+    let arr = []
+
+    while (arr.length <= middle) {
+        const item = !nums2.length || (nums1.length && nums1[0] <= nums2[0]) ? nums1.shift() : nums2.shift()
+
+        arr.push(item)
+    }
+
+    return isEven ? (arr[middle] + arr[middle - 1]) / 2 : arr[middle - 1]
+}
+
+console.log(findMedianSortedArrays([2], []))
